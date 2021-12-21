83bb67881100f4e52dbadee1b9c47dbf09aafa36
MarcinMatuk <marcin.mates@gmail.com>
2021-12-21 14:32:46 +0100

    add app

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Marcin Matuk on 17/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
